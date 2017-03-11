cbd971d14791fe4a408bc82bef466f7d6c5bfd46
Rafal Czernecki <mirrorsluck@gmail.com>
2017-03-11 15:00:22 +0100

    aa

--- zad01/Hello.java
+++ zad01/Hello.java
@@ -2,10 +2,6 @@ package pl.RCZ.zad01;
 public class Hello
 {
 public static void main(String[] args) {
-<<<<<<< HEAD
 	System.out.println("Hello waorld!");
-=======
-	System.out.println("Hello2o world!");
 }
->>>>>>> ffe0de778966d479bd5fb6a0ad3a4ef94cfc0692
 }
